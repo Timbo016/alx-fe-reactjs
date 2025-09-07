3044903d03f2c2c096dd4467418f1e8d5436ee16
Opoola Timileyin <opoolatimileyin@gmail.com>
2025-09-07 05:29:53 +0100

    Updated App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -7,6 +7,7 @@ import WelcomeMessage from './components/WelcomeMessage.jsx';
 import Header from './components/Header.jsx';
 import MainContent from './components/MainContent.jsx';
 import Footer from './components/Footer.jsx';
+import UserProfile from './components/UserProfile';
 /*function App() {
   const [count, setCount] = useState(0)
 
@@ -49,12 +50,24 @@ import Footer from './components/Footer.jsx';
 
 export default App  */
 
-function App() {
+/*function App() {
   return (
     <div>
       <Header />
       <MainContent />
       <Footer />
+    </div>
+  );
+}
+
+export default App;   */
+
+function App() {
+  return (
+    <div>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <UserProfile name="John" age="30" bio="A software developer from Lagos" />
+      <UserProfile name="Mary" age="22" bio="Enjoys cooking and traveling" />
     </div>
   );
 }
